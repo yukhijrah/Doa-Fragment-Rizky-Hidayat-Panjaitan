3a8869222dcc643f5c82b76e29570cfdb6489bd1
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2019-01-06 22:18:50 +0700

    Doa Fragment

---
+++ app/src/main/java/panjaitan/rizky/app/fragmentandroid/adapter/MainActivity.java
@@ -0,0 +1,66 @@
+package panjaitan.rizky.app.kumpulandoa;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import panjaitan.rizky.app.fragmentandroid.R;
+import panjaitan.rizky.app.fragmentandroid.adapter.adapter.ContainerAdapter;
+import panjaitan.rizky.app.fragmentandroid.adapter.fragment.DoaFragment;
+import panjaitan.rizky.app.fragmentandroid.adapter.fragment.HomeFragment;
+import panjaitan.rizky.app.fragmentandroid.adapter.fragment.ProfileFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vpMain;
+    private Button btnHome, btnDoa, btnProfile;
+
+    private ContainerAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //casting object (view)
+        vpMain = (ViewPager) findViewById(R.id.view_pager_main);
+        btnHome = (Button) findViewById(R.id.btn_home);
+        btnDoa = (Button) findViewById(R.id.btn_doa);
+        btnProfile = (Button) findViewById(R.id.btn_profile);
+
+        setContainerPrepared();
+        onButtonsClicked();
+    }
+
+    private void setContainerPrepared(){
+        adapter = new ContainerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), getString(R.string.menu_home));
+        adapter.addFragment(new DoaFragment(), getString(R.string.menu_doa));
+        adapter.addFragment(new ProfileFragment(), getString(R.string.menu_profile));
+        vpMain.setAdapter(adapter);
+    }
+
+    private void onButtonsClicked() {
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                vpMain.setCurrentItem(0);
+            }
+        });
+
+        btnDoa.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                vpMain.setCurrentItem(1);
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                vpMain.setCurrentItem(2);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/panjaitan/rizky/app/fragmentandroid/adapter/model/Jadwal.java
@@ -0,0 +1,21 @@
+package panjaitan.rizky.app.fragmentandroid.adapter.model;
+
+public class Jadwal {
+
+    private String nama;
+    private String waktu;
+
+    public Jadwal(String nama, String waktu) {
+        this.nama = nama;
+        this.waktu = waktu;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+}
